c6e79d620eec92f1e9c7a514e62c80f8fb1e5144
GioMench <giorgiaglm@gmail.com>
2024-02-22 15:52:00 +0100

    try to set js for mail exercise

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,1 +1,29 @@
-console.log('test');
+//console.log('test');
+
+//MAIL 
+
+//chiedo la mail 
+const userMail = prompt('inserisci la tua mail')
+console.log(userMail);
+
+//controllo che la mail sia presente nel mio elenco
+const mailList = ['a@a','b@b', 'c@c', 'd@d']
+
+
+
+for (let i = 0; i = mailList.length; i++) {
+    
+    //IF la mail è presente = message1
+    if (mailList[i] = userMail ){
+        const message1 = 'Welcome Back'
+        console.log(message1);
+    } else {
+        const message2 = 'Please, subscribe your email'
+    }
+    
+}
+
+
+
+
+//ELSE = message2
